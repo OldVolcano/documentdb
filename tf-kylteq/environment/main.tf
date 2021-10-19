9497d04973d249390f41bbabf3f0cc21a32899fa
bill <ozbillwang@gmail.com>
2021-10-19 13:55:10 +1100

    fix the nested provider configuration

--- tf-kylteq/environment/main.tf
+++ tf-kylteq/environment/main.tf
@@ -3,9 +3,15 @@
 ###############################################################################
 terraform {
   required_version = ">= 0.13.0"
+
   required_providers {
-    aws = ">= 3.57.0"
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 3.57.0"
+      configuration_aliases = [ aws.demo, aws.demo2]
+    }
   }
+
 }
 
 ###############################################################################
